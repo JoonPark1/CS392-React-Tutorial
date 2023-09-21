44a696d95bf90b2ed7ffad24d549ee1e79a2de7f
Sung Park <way11142001@gmail.com>
2023-09-21 12:17:29 -0500

    added course form & made sure to prepopulate input fields by default values

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,10 +9,10 @@ const Form = (props) => {
     return (
         <form style={{border: "3px solid black", borderRadius: "3px", marginTop: "20px", width: "50%", height: "75%"}}>
                 <label style = {{paddingRight: "30px", paddingLeft: "20px"}} htmlFor="title">Modify course title:</label>
-                <input style ={{marginBottom: "30px"}} placeholder = {title} type="text" id="title"></input>
+                <input style ={{marginBottom: "30px"}} defaultValue = {title} type="text" id="title"></input>
                 <br /> 
                 <label style = {{paddingRight: "30px", paddingLeft: "20px", marginBottom: "30px"}} htmlFor="meets">Modify meeting time:</label>
-                <input placeholder = {meets} type="text" id="meets"></input>
+                <input defaultValue = {meets} type="text" id="meets"></input>
                 <br /> 
                 <button><Link to = "/">Cancel!</Link></button>
         </form>
