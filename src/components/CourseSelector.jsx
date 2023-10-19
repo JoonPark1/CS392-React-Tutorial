e143b7516d1270f0019303327a7589be1b0d54f9
Sung Park <way11142001@gmail.com>
2023-10-18 20:09:17 -0500

    passed integration test

--- src/components/CourseSelector.jsx
+++ src/components/CourseSelector.jsx
@@ -3,11 +3,11 @@ const CourseSelector = ({checkedTerm, setTerm}) => {
     return (
         <div>
             <input checked = {checkedTerm === "Fall"} id = "Fall" type="radio" onClick={() => setTerm("Fall")}></input>
-            <label htmlFor="Fall">Fall</label>
+            <label data-cy="Fall" htmlFor="Fall">Fall</label>
             <input checked={checkedTerm==="Winter"} id = "Winter" type="radio" onClick={() => setTerm("Winter")}></input>
-            <label htmlFor="Winter">Winter</label>
+            <label data-cy = "Winter" htmlFor="Winter">Winter</label>
             <input checked={checkedTerm==="Spring"} id = "Spring" type="radio" onClick={() => setTerm("Spring")}></input>
-            <label htmlFor="Spring">Spring</label>
+            <label data-cy = "Spring" htmlFor="Spring">Spring</label>
         </div>
     ); 
 }
